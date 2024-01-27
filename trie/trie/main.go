7318e95f8016c9d86fa57ed4be57a2181d1a85ff
Braden Lockwood <braden.lockwood@kiewit.com>
2024-01-27 10:22:06 -0600

    first commit

---
+++ trie/trie/main.go
@@ -0,0 +1,148 @@
+package main
+
+import "fmt"
+import "container/list"
+
+type Node struct {
+	Value     string
+	Word      bool
+	Edges     map[string]*Node
+	CanDelete bool
+}
+
+func PrintNodes(rootNode *Node) {
+	queue := list.New()
+	queue.PushBack(rootNode)
+	for queue.Len() != 0 {
+		size := queue.Len()
+		for i := 0; i < size; i++ {
+			element := queue.Front()
+			queue.Remove(element)
+			node, ok := element.Value.(*Node)
+			if !ok {
+				continue
+			}
+
+			fmt.Print(node.Value + " ")
+			for _, val := range node.Edges {
+				queue.PushBack(val)
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func (node *Node) Insert(value string) {
+	if len(value) == 0 {
+		return
+	}
+	char := string(value[0])
+	if nextNode, ok := node.Edges[string(char)]; ok {
+		nextNode.Insert(value[1:])
+	} else {
+		newNode := &Node{
+			Value: char,
+			Word:  false,
+			Edges: make(map[string]*Node),
+		}
+		node.Edges[char] = newNode
+		newNode.Insert(value[1:])
+	}
+
+	if len(value) == 1 {
+		node.Edges[char].Word = true
+	}
+
+}
+
+/*
+Performe using DFS
+ 1. Navigate to end of the word...
+    - a If node has no children, delete
+    - b If node has children, do not delete but set Word as false
+ 2. After dealing with child node, recurse back up...
+    - a If node does not have any children, delete, else continue
+
+When at a node, check above conditions
+*/
+func (node *Node) Delete(value string) {
+	if len(value) == 0 {
+		return
+	}
+	char := string(value[0])
+	var nextNode *Node
+	var ok bool
+
+	if nextNode, ok = node.Edges[string(char)]; ok {
+		nextNode.Delete(value[1:])
+		if len(value) == 1 {
+			nextNode.Word = false
+		}
+	} else {
+		//Throw error
+		fmt.Println("Node does not exist")
+	}
+
+	//This is where we handle deletion
+	if len(nextNode.Edges) == 0 {
+		nextNode.CanDelete = true
+	} else {
+		child := string(value[1])
+		if nodeChild, ok := nextNode.Edges[child]; ok {
+			if nodeChild.CanDelete {
+				delete(nextNode.Edges, child)
+			}
+			if len(nextNode.Edges) == 0 {
+				nextNode.CanDelete = true
+			}
+		}
+	}
+
+}
+
+func (node *Node) Search(value string) bool {
+	if len(value) == 0 {
+		return false
+	}
+
+	var endOfWord bool
+	var isFound bool
+	if nextNode, ok := node.Edges[string(value[0])]; ok {
+		isFound = nextNode.Search(value[1:])
+		if len(value) == 1 {
+			endOfWord = nextNode.Word
+		}
+
+	}
+
+	return isFound || endOfWord
+
+}
+
+func (node *Node) Content() {
+	fmt.Println(node.Value)
+}
+
+/*
+1. Insertion
+2. Deletion
+3. Lookup
+*/
+func main() {
+	root_node := Node{
+		Value: "root",
+		Word:  false,
+		Edges: make(map[string]*Node),
+	}
+
+	root_node.Insert("app")
+	root_node.Insert("asx")
+	root_node.Insert("asp")
+	root_node.Insert("brad")
+	root_node.Insert("brady")
+	root_node.Insert("brap")
+	PrintNodes(&root_node)
+	root_node.Delete("asp")
+	PrintNodes(&root_node)
+	fmt.Println(root_node.Search("asp"))
+}
---
+++ trie/trie/main_test.go
@@ -0,0 +1,122 @@
+package main
+
+import "testing"
+import "container/list"
+import "sort"
+
+func TestInsert(t *testing.T) {
+	root_node := Node{
+		Value: "root",
+		Word:  false,
+		Edges: make(map[string]*Node),
+	}
+
+	root_node.Insert("app")
+	root_node.Insert("ass")
+	root_node.Insert("aps")
+	root_node.Insert("brad")
+	root_node.Insert("brady")
+	root_node.Insert("brap")
+	root_node.Insert("bred")
+
+	var levels [][]string
+	levels = append(levels, []string{"root"})
+	levels = append(levels, []string{"a", "b"})
+	levels = append(levels, []string{"p", "s", "r"})
+	levels = append(levels, []string{"p", "s", "s", "a", "e"})
+	levels = append(levels, []string{"d", "p", "d"})
+	levels = append(levels, []string{"y"})
+
+	queue := list.New()
+	queue.PushBack(&root_node)
+	level := 0
+	for queue.Len() != 0 { // This counts the level
+		size := queue.Len()
+		current_level_values := []string{}
+		for i := 0; i < size; i++ {
+			element := queue.Front()
+			queue.Remove(element)
+			node, ok := element.Value.(*Node)
+			if !ok {
+				continue
+			}
+
+			current_level_values = append(current_level_values, node.Value)
+
+			for _, val := range node.Edges {
+				queue.PushBack(val)
+			}
+		}
+		level_values := levels[level]
+		sort.Strings(level_values)
+		sort.Strings(current_level_values)
+		if len(level_values) != len(current_level_values) {
+			t.Fatalf(`Level %d: Expected %d nodes, but got %d`, level, len(level_values), len(current_level_values))
+		}
+		for index, _ := range level_values {
+			if level_values[index] != current_level_values[index] {
+				t.Fatalf(`Actual Value: %s; Received Value: %s`, level_values[index], current_level_values[index])
+			}
+		}
+		level++
+	}
+}
+func TestDelete(t *testing.T) {
+	root_node := Node{
+		Value: "root",
+		Word:  false,
+		Edges: make(map[string]*Node),
+	}
+
+	root_node.Insert("app")
+	root_node.Insert("asx")
+	root_node.Insert("aps")
+	root_node.Insert("brad")
+	root_node.Insert("brady")
+	root_node.Insert("brap")
+	root_node.Insert("bred")
+
+	root_node.Delete("asx")
+
+	var levels [][]string
+	levels = append(levels, []string{"root"})
+	levels = append(levels, []string{"a", "b"})
+	levels = append(levels, []string{"p", "r"})
+	levels = append(levels, []string{"p", "s", "a", "e"})
+	levels = append(levels, []string{"d", "p", "d"})
+	levels = append(levels, []string{"y"})
+
+	queue := list.New()
+	queue.PushBack(&root_node)
+	level := 0
+	for queue.Len() != 0 { // This counts the level
+		size := queue.Len()
+		current_level_values := []string{}
+		for i := 0; i < size; i++ {
+			element := queue.Front()
+			queue.Remove(element)
+			node, ok := element.Value.(*Node)
+			if !ok {
+				continue
+			}
+
+			current_level_values = append(current_level_values, node.Value)
+
+			for _, val := range node.Edges {
+				queue.PushBack(val)
+			}
+		}
+		level_values := levels[level]
+		sort.Strings(level_values)
+		sort.Strings(current_level_values)
+		if len(level_values) != len(current_level_values) {
+			t.Fatalf(`Level %d: Expected %d nodes, but got %d`, level, len(level_values), len(current_level_values))
+		}
+		for index, _ := range level_values {
+			if level_values[index] != current_level_values[index] {
+				t.Fatalf(`Actual Value: %s; Received Value: %s`, level_values[index], current_level_values[index])
+			}
+		}
+		level++
+	}
+}
